5e8219039b5e2a4ea2afc3e106968564a4508606
duracell27 <volodymyrshmidt@gmail.com>
2020-09-16 17:51:26 +0300

    popups

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -4,9 +4,13 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 import SearchIcon from '@material-ui/icons/Search';
 import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
-import { relative } from 'path';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import { ModalBlock } from '../components/ModakBlock';
 
-const useClasses = makeStyles((theme) => ({
+
+export const useStylesSignIn = makeStyles((theme) => ({
     wrapper: {
         display: 'flex',
         height: '100vh'
@@ -77,7 +81,23 @@ const useClasses = makeStyles((theme) => ({
 }))
 
 function SignIn() {
-    const classes = useClasses()
+    const classes = useStylesSignIn()
+
+
+    const [visibleModal, setVisibleModal] = React.useState<'signIn' | 'signUp'>();
+
+    const handleClickOpenSignIn = (): void => {
+        setVisibleModal('signIn');
+    };
+
+    const handleClickOpenSignUp = (): void => {
+        setVisibleModal('signUp');
+    };
+
+    const handleCloseModal = (): void => {
+        setVisibleModal(undefined);
+    };
+
 
     return (
         <div className={classes.wrapper}>
@@ -96,11 +116,95 @@ function SignIn() {
                     <TwitterIcon color='primary' className={classes.twitterIcon} />
                     <Typography className={classes.loginSideTitle} variant='h4'>Дізнайтеся, що відбувається у світі просто зараз</Typography>
                     <Typography><b>Приєднуйтеся до Твіттера вже сьогодні.</b></Typography>
-                    <Button className={classes.loginSideButton} variant='contained' color='primary' fullWidth>Зареєструватися</Button>
-                    <Button className={classes.loginSideButton} variant='outlined' color='primary' fullWidth>Увійти</Button>
+                    <Button onClick={handleClickOpenSignUp} className={classes.loginSideButton} variant='contained' color='primary' fullWidth>Зареєструватися</Button>
+                    <Button onClick={handleClickOpenSignIn} className={classes.loginSideButton} variant='outlined' color='primary' fullWidth>Увійти</Button>
                 </div>
             </div>
-            
+            <ModalBlock
+                visible={visibleModal === 'signIn'}
+                onClose={handleCloseModal}
+                classes={classes}
+                title="Войти в аккаунт">
+                <FormControl component="fieldset" fullWidth>
+                    <FormGroup aria-label="position" row>
+                        <TextField
+                            autoFocus
+                            id="email"
+                            label="E-Mail"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="filled"
+                            type="email"
+                            fullWidth
+                        />
+                        <TextField
+                            autoFocus
+                            id="password"
+                            label="Пароль"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="filled"
+                            type="password"
+                            fullWidth
+                        />
+                        <Button onClick={handleCloseModal} variant="contained" color="primary" fullWidth>
+                            Войти
+                </Button>
+                    </FormGroup>
+                </FormControl>
+            </ModalBlock>
+            <ModalBlock
+                visible={visibleModal === 'signUp'}
+                onClose={handleCloseModal}
+                classes={classes}
+                title="Создайте учетную запись">
+                <FormControl  component="fieldset" fullWidth>
+                    <FormGroup aria-label="position" row>
+                        <TextField
+                            
+                            autoFocus
+                            id="name"
+                            label="Имя"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="filled"
+                            type="name"
+                            fullWidth
+                        />
+                        <TextField
+                            
+                            autoFocus
+                            id="email"
+                            label="E-Mail"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="filled"
+                            type="email"
+                            fullWidth
+                        />
+                        <TextField
+                            
+                            autoFocus
+                            id="password"
+                            label="Пароль"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="filled"
+                            type="password"
+                            fullWidth
+                        />
+                        <Button variant="contained" color="primary" fullWidth>
+                            Далее
+                </Button>
+                    </FormGroup>
+                </FormControl>
+            </ModalBlock>
+
         </div>
     )
 }
